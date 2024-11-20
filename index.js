c90f09b9b4077996f9f3735a4b51185377df3411
furqan <furqankhanfk0999@gmail.com>
2024-11-20 18:16:49 +0530

    added winston logger

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
 
 const app = express();
 const port = 8000;
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Furqan khan");
@@ -33,8 +53,7 @@ app.post("/teas", (req, res) => {
   const newtea = { id: value++, name, price };
   teadata.push(newtea);
   res.status(200).send(newtea);
-  console.log(newtea);
-});
+logger.info('A post request is made')});
 
 //updte a tea
 app.put("/teas/:id", (req, res) => {
